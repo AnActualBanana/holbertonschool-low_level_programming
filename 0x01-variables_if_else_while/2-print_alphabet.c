40d126406447033c12f58a11cbbf768d6ebd06ad
AnActualBanana <loganmcclain59@gmail.com>
2022-01-25 18:42:08 -0800

    0x01

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+* This prints every lowercase letter in the alphabet
+*/int main(void)
+{
+char n;
+for (n = 'a' ; n <= 'z' ; n++)
+{
+putchar (n);
+}
+putchar ('\n');
+return (0);
+}
