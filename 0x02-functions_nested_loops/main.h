12255245c59f0a915125a29fcca5327288467b08
AnActualBanana <loganmcclain59@gmail.com>
2022-02-02 06:39:19 -0800

    0x02

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+/* includes */
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* prototypes */
+
+/* _putchar prototype */
+int _putchar(char c);
+
+
+
+#endif /* MAIN_H */
