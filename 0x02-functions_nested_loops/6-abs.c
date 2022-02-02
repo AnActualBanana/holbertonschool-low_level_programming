5c4c472cd27871f1afcc95f2eb42e0e6b3eac329
AnActualBanana <loganmcclain59@gmail.com>
2022-02-02 14:02:29 -0800

    0x02

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+* _abs - prints the absolute value of a number
+* return - 0
+*/int _abs(int r)
+{
+if (r >= 0)
+{
+_putchar(r);
+}
+else
+{
+_putchar(r * (-1));
+}
+return (0);
+}
