2747bbae3d645e7250923418200079b3d6e1feab
AnActualBanana <loganmcclain59@gmail.com>
2022-02-02 10:41:13 -0800

    0x02

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /*
- *
- */
-int _islower(int c)
+* _islower - checks if character is lowercase
+* return - 1 if true, 0 if false
+*/int _islower(int c)
 {
-  if (c >= 97 && c <= 122)
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
