5720b2592cf891b90f4497aed98f148c9578da56
AnActualBanana <loganmcclain59@gmail.com>
2022-02-03 12:01:11 -0800

    0x04

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/*
+* print_square - draws a square on the terminal
+* return - none
+*/void print_square(int size)
+{
+int height;
+int width;
+if (size > 0)
+{
+for (height = 0; height < size; height++)
+{
+for (width = 0; width < size; width++)
+{
+_putchar('#');
+}
+_putchar('\n');
+width = 0;
+}
+}
+else
+{
+_putchar('\n');
+}
+return;
+}
