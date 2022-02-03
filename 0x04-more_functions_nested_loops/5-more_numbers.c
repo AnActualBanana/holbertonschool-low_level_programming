40896fafd268df29bd1042ef886a4398753290a6
AnActualBanana <loganmcclain59@gmail.com>
2022-02-03 10:59:28 -0800

    0x04

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/*
+* more_numbers - prints 10 times the numbers,
+* from 0-14, followed by a new line
+* return - none
+*/void more_numbers(void)
+{
+int i;
+int n;
+for (i = 1; i <= 10; i++)
+{
+for (n = 1; n <= 14; n++)
+{
+if (n >= 10)
+{
+_putchar((n / 10) + '0');
+}
+_putchar((n % 10) + '0');
+}
+_putchar('\n');
+}
+return;
+}
