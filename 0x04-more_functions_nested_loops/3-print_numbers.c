98fee52b645a3cc6fb99a7d2e057660eb23d2cad
AnActualBanana <loganmcclain59@gmail.com>
2022-02-03 09:39:15 -0800

    0x04

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,13 +2,12 @@
 /*
 * print_numbers - prints numbers from 0 to 9
 * return: void
-*/
-void print_numbers(void)
+*/void print_numbers(void)
 {
 int i;
 for (i = 0; i < 10; i++)
 {
-_putchar(i);
+_putchar(('0' + i));
 }
 _putchar('\n');
 }
