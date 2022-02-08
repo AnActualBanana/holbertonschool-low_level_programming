bcc2dcde14f703ed86fd7bb9aae6a3ecca89e3fc
AnActualBanana <loganmcclain59@gmail.com>
2022-02-07 16:52:33 -0800

    0x06

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,11 +11,14 @@ int i;
 int j;
 for (i = 0; dest[i] != '\0'; i++)
 ;
-for (j = 0; ((j < n) && (j < 10)); j++)
+for (j = 0; src[j] != '\0'; j++)
+{
+if (j < n)
 {
 dest[i + j] = src[j];
 }
-if ((j < n) || (j == 10))
+}
+if (j < n)
 {
 dest[i + j] = '\0';
 }
