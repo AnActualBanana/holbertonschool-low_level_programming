ceec7f1f115e69fcb816eec39bc24c8e481e720c
AnActualBanana <loganmcclain59@gmail.com>
2022-02-09 12:39:17 -0800

    0x08

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/*
+* _puts_recursion - prints a string,
+* followed by a new line
+* return - none
+*/void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+s = &s[1];
+_print_rev_recursion(s);
+}
+s = &s[-1];
+_putchar(*s);
+return;
+}
