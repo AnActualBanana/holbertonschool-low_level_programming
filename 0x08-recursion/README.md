eef097bebf04fe2a1e56e0968e1a58a1f29dfb48
AnActualBanana <loganmcclain59@gmail.com>
2022-02-09 10:49:04 -0800

    0x08

---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+for project 0x08 C - Recursion
---
+++ 0x08-recursion/question.c
@@ -0,0 +1,12 @@
+/* this isn't a program, just a question */
+void printing_characters_from_a_string(char *s)
+{int i;
+
+  for (i = 0; s[i] != '\0'; i++)
+    {
+
+    }
+
+
+     return;
+     }
