5f1adf3ae6e48644115956e5a5a20b9929e00f07
AnActualBanana <loganmcclain59@gmail.com>
2022-02-15 09:37:39 -0800

    0x0A

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/*
+* main - prints its name, followed by a new line
+* return - 0
+*/int main(int argc, char *argv[])
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+for project 0x0A-argc_argv
