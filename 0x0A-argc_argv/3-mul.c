b43569217d6a68e5d03a3e5e39a1c85182a72412
AnActualBanana <loganmcclain59@gmail.com>
2022-02-15 10:24:28 -0800

    0x0A

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/*
+* main - multiplies two numbers
+* return - 0 if two arguments given, -1 if else
+*/int main(int argc, char *argv[])
+{
+if (argc == 3)
+{
+int number_1;
+int number_2;
+int product;
+number_1 = (int)*argv[1];
+number_2 = (int)*argv[2];
+product = ((number_1 - '0') * (number_2 - '0'));
+printf("%d\n", product);
+return (0);
+}
+else
+{
+printf("Error\n");
+return (-1);
+}
+}
