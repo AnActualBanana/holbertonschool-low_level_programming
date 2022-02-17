770179b51f01680d8dfb3fc26c8c3608bd549c43
AnActualBanana <loganmcclain59@gmail.com>
2022-02-17 11:08:48 -0800

    0x0C

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,15 +2,23 @@
 /*
 * *create_array - creates an array of chars,
 * and initializes it with a specific char
-* return - NULL if seize is 0, pointer to array otherwise
+* return - NULL if size is 0, pointer to array otherwise
 */char *create_array(unsigned int size, char c)
 {
-char *a;
-a = &c;
-a = (char *)malloc(4);
+char *array = NULL;
+unsigned int count;
+array = (char *)malloc(size * sizeof(c));
+if (array == NULL)
+{
+return (NULL);
+}
+for (count = 0; count < size; count++)
+{
+array[count] = c;
+}
 if (size == 0)
 {
 return (NULL);
 }
-return (a);
+return (array);
 }
