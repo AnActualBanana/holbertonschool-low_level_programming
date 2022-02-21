8f421ccce85805e2bfa5a5d3fb6df922cc9490a5
AnActualBanana <loganmcclain59@gmail.com>
2022-02-21 14:12:42 -0800

    0x0E

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /*
-* *malloc_chjecked - allocates memory
+* *malloc_checked - allocates memory
 * return - 0 if normal, 1 if fail
 */void *malloc_checked(unsigned int b)
 {
-int *a = malloc(1 * sizeof(b));
+int *a = malloc(b);
 if (a == NULL)
 {
 exit(98);
