3a3a6ccdf51cf625456eeda93fd280ef417a2346
AnActualBanana <loganmcclain59@gmail.com>
2022-02-25 12:15:39 -0800

    0x11

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+/*
+* print_numbers - prints numbers, followed by a new line
+* return - none
+*/void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int count, num;
+va_list args;
+if (args == 0 || n == 0 || separator == NULL)
+{
+return;
+}
+va_start(args, n);
+num = 0;
+for (count = 0; count < n; count++)
+{
+num = va_arg(args, int);
+printf("%d", num);
+if (count != (n - 1))
+{
+printf("%s", separator);
+}
+}
+va_end(args);
+printf("\n");
+return;
+}
