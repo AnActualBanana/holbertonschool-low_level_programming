5b1e9b6996f5463e10e3a250226c8549a3b4e78d
AnActualBanana <loganmcclain59@gmail.com>
2022-02-25 12:25:22 -0800

    0x11

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+/*
+* print_numbers - prints numbers, followed by a new line
+* return - none
+*/void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int count;
+char *string;
+va_list args;
+if (args == 0)
+{
+return;
+}
+va_start(args, n);
+for (count = 0; count < n; count++)
+{
+string = va_arg(args, char *);
+if (string == NULL)
+{
+printf("(nil");
+}
+else
+{
+printf("%s", string);
+}
+if (count != (n - 1) && separator != NULL)
+{
+printf("%s", separator);
+}
+}
+va_end(args);
+printf("\n");
+return;
+}
