e2746e08d9815821ec2d72c250283b0fdb7391ac
AnActualBanana <loganmcclain59@gmail.com>
2022-03-09 12:40:02 -0800

    task 3.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,6 +15,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	new->str = strdup(str);
 	new->len = strlen(str);
-	*head->next = new;
+	new->next = *head;
+	*head = new;
 	return (new);
 }
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,24 +1,27 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
+ * add_node_end - adds a new node at the end of a list_t list
+ * @head: head of the first node in the list
+ * @str: the string to populate the new node with
+ * Return: the address of the new element, or NULL if it failed
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new = malloc(1 * sizeof(list_t));
+	list_t *iterate = malloc(1 * sizeof(list_t));
 
-	if (new == NULL)
+	iterate->next = *head;
+	if (new == NULL || iterate == NULL)
 	{
 		return (NULL);
 	}
-	while (*head->next != NULL)
+	while (iterate->next != NULL)
 	{
-		*head = *head->next;
-	}
+		iterate = iterate->next;
+		}
+	iterate->next = new;
 	new->str = strdup(str);
 	new->len = strlen(str);
-	*head->next = new;
-	return(new);
+	new->next = NULL;
+	return (new);
 }
