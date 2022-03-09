6c2f4be8fab258beb8475ad0b1cd02bb9eacd85e
AnActualBanana <loganmcclain59@gmail.com>
2022-03-09 11:16:00 -0800

    0x13

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,10 +14,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		return (NULL);
-	}
 	new->len = strlen(str);
 	new->next = *head;
 	*head = new;
