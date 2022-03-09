0a9a58059c632e37f0abefefbcf3b4496f9203c4
AnActualBanana <loganmcclain59@gmail.com>
2022-03-09 08:45:44 -0800

    0x13

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,15 +7,9 @@
 unsigned int count = 0;
 while (h->next != NULL)
 {
-if (h->str != NULL && h->len != 0)
-{
 count++;
-}
 h = h->next;
 }
-if (h->str != NULL && h->len != 0)
-{
 count++;
-}
 return (count);
 }
