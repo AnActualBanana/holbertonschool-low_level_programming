42e44b5f235e3e7c8e0c8b013965a1cdaf3be7c4
AnActualBanana <loganmcclain59@gmail.com>
2022-03-11 11:49:29 -0800

    0x15

---
+++ 0x14-bit-manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <math.h>
+/**
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int count = 0;
+	unsigned int cur_digit = 0;
+	int rev_count = 0;
+	unsigned int pow_mul = 1;
+	unsigned int sum = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (count = 0; b[count] != '\0'; count++)
+	{
+		if (b[count] != '1' && b[count] != '0')
+		{
+			return (0);
+		}
+	}
+	for (rev_count = 1; (count - rev_count) > (-1); rev_count++)
+		{
+			cur_digit = (b[(count - rev_count)] - '0');
+			if (cur_digit == 1)
+			{
+				sum += pow_mul;
+			}
+			pow_mul *= 2;
+		}
+	return(sum);
+}
---
+++ 0x14-bit-manipulation/README.md
@@ -0,0 +1,1 @@
+for project 0x15 C - Bit manipulation
