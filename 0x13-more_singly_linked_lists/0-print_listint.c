e310a70de71cae197eb8419a57301c5811d774f6
AnActualBanana <loganmcclain59@gmail.com>
2022-03-14 12:27:58 -0700

    0x14

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
-* print_list - prints all the elements of a list_t list
-* return - the number of nodes
+* print_listint - prints all the elements of a list_t list
+* @h: head of the list
+* Return: the number of nodes
 */
 size_t print_listint(const listint_t *h)
 {
