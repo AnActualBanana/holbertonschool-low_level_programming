f3fca245d992ad29f759550942e6f1b466c215e9
AnActualBanana <loganmcclain59@gmail.com>
2022-03-17 09:41:05 -0700

    fix code check on task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	wr = write(op, text_content, strlen(text_content));
+	if (text_content != NULL)
+		wr = write(op, text_content, strlen(text_content));
 	if (wr == -1)
 		return (-1);
 	cl = close(op);
