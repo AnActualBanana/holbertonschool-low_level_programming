e5ddaf2f4fa44d585a43d2d831c8a27e953f03e3
AnActualBanana <loganmcclain59@gmail.com>
2022-04-04 13:56:56 -0700

    for task 3, fix

--- 0x18-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *last;
 
 	new = malloc(1 * sizeof(dlistint_t));
-	if (new == NULL || head == NULL)
+	if (new == NULL)
 		return (NULL);
 	if (*head == NULL)
 	{
