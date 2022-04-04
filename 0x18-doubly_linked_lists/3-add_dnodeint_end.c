db9261f0f896eb342a13b1b29aa698ab342dbc1e
AnActualBanana <loganmcclain59@gmail.com>
2022-04-04 13:55:50 -0700

    for task 3

---
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list.
+ * @head: the head of the list
+ * @n: an integer value to be stored in the new node
+ * Return: the address of the new element, or NULL if it failed.
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	dlistint_t *last;
+
+	new = malloc(1 * sizeof(dlistint_t));
+	if (new == NULL || head == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	last = *head;
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
+	new->prev = last;
+	new->next = NULL;
+	new->n = n;
+	return (new);
+}
