34939fd471093bb48785b4e22a27f2ab0a4c7241
AnActualBanana <loganmcclain59@gmail.com>
2022-04-06 08:18:53 -0700

    fix task 0?

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,10 +7,17 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned int count;
 
 	table = malloc(1 * sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
+	for (count = 0; count < size; count++)
+	{
+		table->array = malloc(1 * sizeof(hash_node_t));
+		if (table->array == NULL)
+			return (NULL);
+	}
 	return (table);
 }
