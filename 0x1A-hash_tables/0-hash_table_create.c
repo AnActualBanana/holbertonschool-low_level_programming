f0bdf4fe47ec504d3742376e668d7d9bae80ebca
AnActualBanana <loganmcclain59@gmail.com>
2022-04-06 08:25:44 -0700

    one more check

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,6 @@
 #include "hash_tables.h"
 /**
- * hash_tale_create - creates a hash table.
+ * hash_table_create - creates a hash table.
  * @size: the size of the array of nodes.
  * Return: a pointer to the newly created hash table.
  */
@@ -13,9 +13,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->size = size;
+	table->array = malloc(size * sizeof(hash_table_t));
+	if (table->array == NULL)
+		return (NULL);
 	for (count = 0; count < size; count++)
 	{
-		table->array = malloc(1 * sizeof(hash_node_t));
+		table->array[count] = malloc(1 * sizeof(hash_node_t));
 		if (table->array == NULL)
 			return (NULL);
 	}
