7dfb8362ebd405185b1eb098e1c815a98bb263b0
AnActualBanana <loganmcclain59@gmail.com>
2022-04-06 08:34:45 -0700

    removed loop

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	unsigned int count;
 
 	table = malloc(1 * sizeof(hash_table_t));
 	if (table == NULL)
@@ -16,11 +15,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table->array = malloc(size * sizeof(hash_node_t));
 	if (table->array == NULL)
 		return (NULL);
-	for (count = 0; count < size; count++)
-	{
-		table->array[count] = malloc(1 * sizeof(hash_node_t));
-		if (table->array == NULL)
-			return (NULL);
-	}
 	return (table);
 }
