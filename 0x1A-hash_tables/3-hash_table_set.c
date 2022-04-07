32152d6ccb5a3fc3f5003a942480cc6fbd8f9451
AnActualBanana <loganmcclain59@gmail.com>
2022-04-07 06:22:16 -0700

    task 6

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table to add or update the key/value to
+ * @key: the key. Can not be an empty string.
+ * @value: the value associated the the key.
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+}
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,25 @@
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_table_t *head;
+	hash_node_t *node, *temp;
+	unsigned long int count;
+
+	head = ht;
+	for (count = 0; count < ht->size; count++)
+	{
+		if (ht->array[count] != NULL)
+		{
+			node = ht->array[count];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}
