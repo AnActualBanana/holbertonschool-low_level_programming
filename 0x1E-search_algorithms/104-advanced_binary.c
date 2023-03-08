a50108190bfbe616b09514c9f4982d282f8bfa29
AnActualBanana <loganmcclain59@gmail.com>
2023-03-08 10:04:49 -0600

    revert correctly

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -3,45 +3,12 @@
 #include <stdio.h>
 
 /**
-<<<<<<< HEAD
- * advanced_binary - Searches for a value in a sorted array of integers.
- *
- * @array: Pointer to the first element of the array to search in.
- * @size: Number of elements in the array.
- * @value: Value to search for.
-=======
  * advanced_binary - searches for a value in a sorted array of integers.
->>>>>>> parent of 2fec6b6 (new version)
  *
  * Return: Index where the value was found or -1 if it fails.
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-<<<<<<< HEAD
-	size_t i, mid;
-	int res;
-
-	if (array == NULL || size == 0)
-		return (-1);
-
-	printf("Searching in array: ");
-	for (i = 0; i < size; i++)
-		printf("%d%s", array[i], i == size - 1 ? "\n" : ", ");
-
-	mid = (size - 1) / 2;
-	if (array[mid] == value)
-	{
-		if (mid == 0 || array[mid - 1] != value)
-			return (mid);
-	}
-
-	if (array[mid] >= value)
-		return (advanced_binary(array, mid + 1, value));
-
-	res = advanced_binary(array + mid + 1, size - mid - 1, value);
-	return (res == -1 ? -1 : res + mid + 1);
-}
-=======
     if (array == NULL || size == 0)
         return (-1);
 
@@ -75,4 +42,3 @@ int advanced_binary(int *array, size_t size, int value)
 
     return (-1);
 }
->>>>>>> parent of 2fec6b6 (new version)
